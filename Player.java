7c6719935bee5cf5939082ee265c4feab72caeae
Zehra Erdem <63507502+ZehraErdem@users.noreply.github.com>
2022-03-13 22:43:55 +0300

    Create Player.java

---
+++ Player.java
@@ -0,0 +1,22 @@
+public class Player {
+
+    //...
+    int noOfAwards = 0;
+    boolean won = false;
+
+    public int getNoOfAwards() {
+        return noOfAwards;
+    }
+
+    public void setNoOfAwards(int noOfAwards) {
+        this.noOfAwards = noOfAwards;
+    }
+
+    public boolean isWon() {
+        return won;
+    }
+
+    public void setWon(boolean won) {
+        this.won = won;
+    }
+}
